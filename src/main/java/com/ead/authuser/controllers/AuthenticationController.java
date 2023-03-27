7517eb7e9f87845e45d9d0a1f162f24ef4a17064
luizhenriqque18 <luizhenriqque15@gmail.com>
2023-03-26 23:18:31 -0400

    feat: implement controller auth with signup

---
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -0,0 +1,45 @@
+package com.ead.authuser.controllers;
+
+import com.ead.authuser.dtos.UserDto;
+import com.ead.authuser.enums.UserStatus;
+import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.UserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    private UserService userService;
+
+    public AuthenticationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Object> registerUser(@RequestBody UserDto userDto) {
+        if(userService.existsByUsername(userDto.getUsername())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is Already Taken!");
+        }
+        if(userService.existsByEmail(userDto.getEmail())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is Already Taken!");
+        }
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userDto, userModel);
+        userModel.setUserStatus(UserStatus.ACTIVE);
+        userModel.setUserType(UserType.STUDENT);
+        userModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        userModel.setLastUpateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        this.userService.save(userModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
+    }
+}
