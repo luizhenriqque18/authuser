85873a9e44ad495136d15061cdb2c49804727131
luizhenriqque18 <luizhenriqque15@gmail.com>
2023-03-26 22:19:15 -0400

    feat: implement service user with findAll, findById and delete

--- src/main/java/com/ead/authuser/services/UserService.java
+++ src/main/java/com/ead/authuser/services/UserService.java
@@ -1,3 +1,18 @@
 package com.ead.authuser.services;
 
-public interface UserService { }
+import com.ead.authuser.models.UserModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserService {
+    List<UserModel> findAll();
+
+    Optional<UserModel> findById(UUID userId);
+
+    void delete(UserModel userModel);
+}
--- src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -1,8 +1,13 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserRepository;
 import com.ead.authuser.services.UserService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -10,5 +15,20 @@ public class UserServiceImpl implements UserService {
 
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<UserModel> findAll() {
+        return this.userRepository.findAll();
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        return this.userRepository.findById(userId);
+    }
+
+    @Override
+    public void delete(UserModel userModel) {
+        this.userRepository.delete(userModel);
     }
 }
