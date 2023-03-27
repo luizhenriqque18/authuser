cecdf95088742725a5c570c01b0f3cb6fe13cda8
luizhenriqque18 <luizhenriqque15@gmail.com>
2023-03-26 23:15:49 -0400

    fix: typing error

--- src/main/java/com/ead/authuser/models/UserModel.java
+++ src/main/java/com/ead/authuser/models/UserModel.java
@@ -3,6 +3,7 @@ package com.ead.authuser.models;
 import com.ead.authuser.enums.UserStatus;
 import com.ead.authuser.enums.UserType;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
@@ -24,7 +25,7 @@ public class UserModel implements Serializable {
     private String username;
     @Column(nullable = false, unique = true, length = 50)
     private String email;
-    @JsonInclude
+    @JsonIgnore
     @Column(nullable = false, length = 255)
     private String password;
     @Column(nullable = false, length = 150)
