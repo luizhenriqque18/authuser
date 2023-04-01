2e14ea466b56513becfb4c28f708da1aa2a32cef
luizhenriqque18 <luizhenriqque15@gmail.com>
2023-04-01 17:39:46 -0400

    feat: Paginação em APIs com Spring Data

--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -4,6 +4,10 @@ import com.ead.authuser.dtos.UserDto;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -26,10 +30,9 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping
-    public ResponseEntity<List<UserModel>> getAllUsers() {
-        var users = this.userService.findAll();
-
+    @GetMapping()
+    public ResponseEntity<Page<UserModel>> getAllUsers(@PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable) {
+        Page<UserModel> users = this.userService.findAll(pageable);
         return ResponseEntity.status(HttpStatus.OK).body(users);
     }
 
